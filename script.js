dede8bfded2b843470725648395b681a28b6efa9
magdy Abo El-nour <163665800+magdy246@users.noreply.github.com>
2024-06-28 11:17:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-$("section").animation
+$("section").animation({width:100%},2000)
+$("section").animation({height:100vh},2000, function {
+$("h1").fedin(2000, function {
+$(".col-md-4").slideDown(2000)
+})
+})
